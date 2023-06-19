46cd062dff2eb01dee0ce2a06e7e23773b64402e
Husseinelassy <https://github.com/hussien450/HIS.git>
2023-06-20 02:01:53 +0300

    lasssstttttt

--- qr.php
+++ qr.php
@@ -7,22 +7,27 @@
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/qr.css">
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+<header>
+        <div class="logo_image"> <img src="css/images/logo2.png" alt="HIS Logo" width="80" height="80"> </div>
+        <a href="#" class="logo">Hospital Information System</a>
+        <nav class="navigation">
+            <a href="#map">The Map</a>
+            <a href="#Patient-Search">Patient Search</a>
+            <a href="#Staff-Search">Staff Search</a>
+            <a href="#contact">Contact</a>
+            <a href="Policies.html">Policies</a>
+            <a href="qr.php">QR Generator</a>
+            <a href="logout.php">Logout</a>
+        </nav>
+    </header>
 
 
-
-<div class="container">
-	<h1 class="page-header text-center">Get patient Qr code</h1>
+<div class="main">
+	
 	<div class="row">
-		<div class="col-sm-3 col-sm-offset-3">
+		<div class="">
 			<form method="POST">
 				<div class="form-group">
 					<label for="">PID</label>
@@ -31,7 +36,7 @@
 				<button type="submit" class="btn " btn-primary  name="generate">Generate QRCode</button>
 			</form>
 		</div>
-		<div class="col-sm-3">
+		<div class="">
 			<?php
             include "connect2.php";
             session_start();
@@ -58,7 +63,8 @@
 		</div>
 	</div>
 </div>
-
+<div class="position">
+<h1 class="page-header text-center">Get patient Qr code</h1>
 <a href='patient-information.php'><?php
                         $_SESSION['patient-case'] = $row['case'];
                         $_SESSION['patient-name'] = $row['firstname'];
@@ -67,14 +73,14 @@
                         $_SESSION['patient-address'] = $row['address'];
                         $_SESSION['patient-phone'] = $row['phone'];
                         $_SESSION['patient-email'] = $row['email'];
-                        $_SESSION['patient-Bill'] = $row['BillAmount']; ?><img src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=<?php echo "http://localhost/him/him/patient-information.php"?>&choe=UTF-8'></a>
+                        $_SESSION['patient-Bill'] = $row['BillAmount']; ?>  <img class="QRIMG" src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=<?php echo "http://localhost/him/him/patient-information.php"?>&choe=UTF-8'></a>
 				<?php
                     }}else {
                         echo "ID doesn't exist";
                     }
                 }
                     ?>
-
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
